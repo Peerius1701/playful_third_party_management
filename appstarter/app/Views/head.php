ff8416741dae7553713bc5e59018483e46fb10eb
Peerius1701 <62801386+Peerius1701@users.noreply.github.com>
2025-02-11 21:06:19 +0100

    Update head.php

--- appstarter/app/Views/head.php
+++ appstarter/app/Views/head.php
@@ -5,7 +5,7 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-<script src="#LINK REMOVED FOR SECURITY" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/#LINK_REMOVED" crossorigin="anonymous"></script>
 <script src="<?php echo base_url('/js/jquery-3.6.1.min.js'); ?>"></script>
 <script src="<?php echo base_url('/js/popper.min.js'); ?>"></script>
 <script src="<?php echo base_url('/js/bootstrap.min.js'); ?>"></script>
